1e0d4df6336f8e5304c152b58e97e42246aaadcc
Bob He <bobhe@csilvm-07.cs.ucsb.edu>
2021-06-13 03:04:38 -0700

    com

--- contactformhandler.php
+++ contactformhandler.php
@@ -3,14 +3,17 @@
     $visitor_email = $_POST['email'];
     $message = $_POST['msg'];
 
-    $email_from = 'Zeta Website'
+    $email_from = 'Zeta Website';
 
-    $email_subject = "Zeta Inquiries"
+    $email_subject = "Zeta Inquiries";
 
     $email_body = "Name: $name.\n"
                     "Email: $visitor_email.\n"
-                        "Message: $message.\n"
+                        "Message: $message.\n";
 
-    $to = "roberthe9606@gmail.com"
+    $to = "roberthe9606@gmail.com";
 
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("Location: index.html");
 ?>
