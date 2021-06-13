309c557cf8b55e75ed630cd243665e36ac5df66c
Bob He <bobhe@csilvm-07.cs.ucsb.edu>
2021-06-13 03:09:43 -0700

    comm

--- contactformhandler.php
+++ contactformhandler.php
@@ -13,7 +13,10 @@
 
     $to = "roberthe9606@gmail.com";
 
-    $headers = "From : $email_from \r\n";
+    $headers .= "From : $email_from \r\n";
+
+    $headers .= "Reply-To: $visitor_email \r\n";
+
 
     mail($to,$email_subject,$email_body,$headers);
 
